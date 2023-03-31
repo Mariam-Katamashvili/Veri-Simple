e686bf701b3ca66c26d66e3f84e2caebbfe0f1af
Mariam-Katamashvili <127763064+Mariam-Katamashvili@users.noreply.github.com>
2023-03-31 16:02:04 +0400

    Add files via upload

---
+++ Full Adder/tb.v
@@ -0,0 +1,42 @@
+module tb();
+reg a,b,c;
+wire o;
+fulladder circ(a,b,c,o);
+
+initial begin
+
+a = 0;
+b = 0;
+c = 0;
+#5;
+a = 0;
+b = 0;
+c = 1;
+#5;
+a = 0;
+b = 1;
+c = 0;
+#5;
+a = 0;
+b = 1;
+c = 1;
+#5;
+a = 1;
+b = 0;
+c = 0;
+#5;
+a = 1;
+b = 0;
+c = 1;
+#5;
+a = 1;
+b = 1;
+c = 0;
+#5;
+a = 1;
+b = 1;
+c = 1;
+#5;
+
+end
+endmodule
